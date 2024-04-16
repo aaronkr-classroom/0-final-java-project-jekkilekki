05a51b526a41d9e837909c20b975852e95275903
Aaron Snowberger <jekkilekki@gmail.com>
2024-04-16 17:26:29 +0900

    Until interface

--- src/CartItem.java
+++ src/CartItem.java
@@ -1,28 +1,31 @@
 public class CartItem {
 	// 변수
-	private String[] itemBook = new String[7];
+	private Book itemBook; // 9장
 	private String bookID;
 	private int quantity;
 	private int totalPrice;
+	
 	// 생성자
 	public CartItem() { }
-	public CartItem(String[] book) {
-		this.itemBook = book;
-		this.bookID = book[0];
+	public CartItem(Book booklist) { // 9장
+		this.itemBook = booklist;
+		this.bookID = booklist.getBookId();
 		this.quantity = 1;
 		updateTotalPrice();
 	}
+	
 	// Getter 메서드
-	public String[] getItemBook() { return itemBook; }
+	public Book getItemBook() { return itemBook; } // 9장
 	public String getBookID() { return bookID; }
 	public int getQuantity() { return quantity; }
 	public int getTotalPrice() { return totalPrice; }
 	
 	// Setter 메서드
-	public void setItemBook(String[] itemBook) { this.itemBook = itemBook;}
+	public void setItemBook(Book itemBook) { this.itemBook = itemBook; } // 9장
 	public void setBookID(String bookID) { this.bookID = bookID; }
 	public void setQuantity(int quantity) { this.quantity = quantity; }
+	public void setTotalPrice(int totalPrice) { this.totalPrice = totalPrice; } // 9장
 	public void updateTotalPrice() {
-		totalPrice = Integer.parseInt(this.itemBook[2]) * this.quantity;
+		totalPrice = this.itemBook.getUnitPrice() * this.quantity; // 9장
 	}
 }
